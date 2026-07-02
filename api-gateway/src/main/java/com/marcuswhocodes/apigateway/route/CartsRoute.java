74d3607c4afdca819319824356be0989aee4878a
marc-bhunu <macbhunu@gmail.com>
2026-07-02 15:43:22 +0200

    feat: added routes to all projects on the api gateway

---
+++ api-gateway/src/main/java/com/marcuswhocodes/apigateway/route/CartsRoute.java
@@ -0,0 +1,23 @@
+package com.marcuswhocodes.apigateway.route;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions.uri;
+import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
+import static org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions.http;
+
+@Configuration
+public class CartsRoute {
+
+    @Bean
+    public RouterFunction<ServerResponse> cartRoute() {
+        return route("cart-route")
+                .route(RequestPredicates.path("/api/v1/cart/**"), http())
+                .before(uri("http://localhost:8081"))
+                .build();
+    }
+}
